226b8724a527aad8e37bc81a298ede301aba4f28
Sebastien <sebastien.vuong.teamcanada@gmail.com>
2017-04-17 22:13:43 +0000

    Completed exercise 4... god damn those 8'000 bugs.

--- index.js
+++ index.js
@@ -1,15 +1,102 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var mysql = require('promise-mysql');
+
 var app = express();
+
+var connection = mysql.createPool({
+    host     : 'localhost',
+    user     : 'root', // CHANGE THIS :)
+    password : '',
+    database: 'reddit_api',
+    connectionLimit: 10
+});
+
+//app.use(bodyParser.urlencoded({extended: true}));
+
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+// EXERCISE 1
+// app.get('/hello', function(request, response) {
+//   response.send('<h1>Hello World!</>');
+// })
+
+
+// EXERCISE 2
 app.get('/hello', function(request, response) {
-  response.end('<h1>Hello World!</>');
+    
+    if (request.query.name) {
+        response.send('<h1>Hello ' + request.query.name +'</h1>');
+    }
+    else {
+        response.send('<h1>Hello World!</>');
+    }
+});
+
+
+// EXERCISE 3
+app.get('/calculator/:operation', function(request, response) {
+    // console.log(request.query.num1);
+    // console.log(request.query.num2);
+    // console.log(request.params);
+    var num1 = parseInt(request.query.num1);
+    var num2 = parseInt(request.query.num2);
+    var output = {
+        "operation" : request.params.operation,
+        "firstOperand": num1,
+        "secondOperand": num2
+    }
+    switch(request.params.operation) {
+        case "add":
+            output.solution = num1 + num2;
+            break;
+        case "multiply":
+            output.solution = num1 * num2;
+            break;
+        default:
+            output.firstOperand = 0;
+            output.secondOperand = 0;
+            response.status('400 Bad Request');
+   }
+   response.send(output);
+});
+
+
+// EXERCISE 4
+app.get('/posts', function(request, response) {
+    var RedditAPI = require('../reddit-nodejs-api/reddit.js');
+    var myReddit = new RedditAPI(connection);
+    myReddit.getAllPosts().then(function(result) {
+        var output = 
+            `<div id="posts">
+            <h1>List of posts</h1>
+            <ul class="posts-list">`;
+        for (var i = 0; i < result.length; i++) {
+                output += 
+                `
+                <li class="post-item">
+                    <h2 class="post-item__title">
+                        <a href="http://the.post.url.value/">` + result[i].post_title + `</a>
+                    </h2>
+                    <p>Created by ` + result[i].user.username + `</p>
+                </li>
+                `;
+            //console.log(temp + 'SINRIVER')
+            //output += 'a';
+            //console.log(output)
+        }
+        output +=
+            `   
+                </ul>
+                </div>
+            `;                                              
+        console.log(output);
+        response.send(output);
+    })
 })
-
-
 
 
 /* YOU DON'T HAVE TO CHANGE ANYTHING BELOW THIS LINE :) */
